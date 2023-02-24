2f0ee1b4f1b43192728298f627a49b9bf5a75a5b
Geraldo Neto <geraldodoliveira123@gmail.com>
2023-02-24 17:43:54 -0300

    Criando projeto

---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Geraldo",
+  },
+  {
+    id: 2,
+    name: "Ewerton",
+  },
+  {
+    id: 3,
+    name: "Josemar",
+  },
+  {
+    id: 4,
+    name: "Gustavo",
+  },
+];
